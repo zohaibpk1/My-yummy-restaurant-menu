7e4a4c632e9b88f587b4d98fdedd2179fc65746e
Zohaib Raza <111698841+zohaibpk1@users.noreply.github.com>
2026-07-28 16:09:45 +0500

    Refactor ordering process and total calculation
    Added functionality to handle multiple orders and total calculation.

--- restu.py
+++ restu.py
@@ -29,3 +29,11 @@ if another_order == 'yes':
     else:
         print(f"Sorry, Your Ordered item {item_2} is not available in our menu.")
     print(f"The total amount of items to pay is {order_total} ")
+
+if another_order == 'no':
+    item_2 = input("Enter the name of your second item order = ")
+    if item_2 in menu:
+        order_total += menu[item_2]
+        print(f"your item {item_2} has been added to your order")
+    else:
+        print(f"the total amount of item to pay is {order_total}")
